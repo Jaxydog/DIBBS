006445f2b10c77ed5c7adb089bb1e79b1a9678a5
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-06-02 11:28:31 -0400

    Update modal builder

--- source/builder/modal.ts
+++ source/builder/modal.ts
@@ -1,7 +1,14 @@
-import { MessageActionRow, Modal, ModalActionRowComponent, ModalActionRowComponentResolvable } from "discord.js"
+import {
+	MessageActionRow,
+	Modal,
+	ModalActionRowComponent,
+	TextInputComponent,
+	TextInputStyleResolvable,
+} from "discord.js"
 
 export class ModalBuilder {
 	private __modal: Modal = new Modal()
+	private __row = new MessageActionRow<ModalActionRowComponent>()
 
 	public setId(id: string) {
 		this.__modal.setCustomId(id)
@@ -11,12 +18,49 @@ export class ModalBuilder {
 		this.__modal.setTitle(title)
 		return this
 	}
-	public setComponents(...components: ModalActionRowComponentResolvable[]) {
-		const row = new MessageActionRow<ModalActionRowComponent>().setComponents(...components.slice(0, 5))
-		this.__modal.setComponents(row)
+	public addField(field: ModalFieldBuilder) {
+		if (this.__row.components.length < 5) {
+			this.__row.addComponents(field.build())
+		}
 		return this.__modal
 	}
 	public build() {
 		return this.__modal
+	}
+}
+export class ModalFieldBuilder {
+	private __field = new TextInputComponent()
+
+	public setId(id: string) {
+		this.__field.setCustomId(id)
+		return this
+	}
+	public setTitle(title: string) {
+		this.__field.setLabel(title)
+		return this
+	}
+	public setStyle(style: TextInputStyleResolvable) {
+		this.__field.setStyle(style)
+		return this
+	}
+	public setBounds(min: number, max: number) {
+		this.__field.setMinLength(min)
+		this.__field.setMaxLength(max)
+		return this
+	}
+	public setPlaceholder(placeholder: string) {
+		this.__field.setPlaceholder(placeholder)
+		return this
+	}
+	public setDefault(value: string) {
+		this.__field.setValue(value)
+		return this
+	}
+	public setRequired(required = true) {
+		this.__field.setRequired(required)
+		return this
+	}
+	public build() {
+		return this.__field
 	}
 }
